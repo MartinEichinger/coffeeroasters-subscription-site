4061b12138a93fed6d9efeb26cfba08b4c1e6a38
EdMadd <martl.eichinger@gmail.com>
2022-02-27 18:46:28 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import About from "./components/About/About";
+import Plan from "./components/Plan/Plan";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Nav />
+
+			<Routes>
+				<Route path="/about" element={<About />} />
+				<Route path="/plan" element={<Plan />} />
+				<Route path="/" element={<Home />} />
+			</Routes>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,57 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import { jsx } from "@emotion/react";
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  const [visible, setVisible] = useState(0);
+
+  return (
+    <React.Fragment>
+      <nav className="d-flex flex-row align-items-center justify-content-between">
+        <div>
+          <a href="/">
+            <img src="./img/nav/logo.svg" alt="logo coffeeroasters" />
+          </a>
+        </div>
+        <div className="nav-icon d-block d-sm-none" onClick={() => (visible ? setVisible(0) : setVisible(1))}>
+          <img src="./img/nav/icon-hamburger.svg" alt="logo mobile menu" />
+        </div>
+        <ul className="nav d-none d-sm-flex justify-content-end flex-row">
+          <li className="nav-link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="/about">About Us</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="/plan">Create your plan</Link>
+          </li>
+        </ul>
+      </nav>
+      <div
+        className={
+          visible
+            ? "nav-menu d-flex d-sm-none flex-column align-items-center slide-in-top"
+            : "nav-menu d-flex d-sm-none flex-column align-items-center slide-out-top"
+        }
+      >
+        <ul>
+          <li className="nav-link" onClick={() => setVisible(0)}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-link" onClick={() => setVisible(0)}>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li className="nav-link" onClick={() => setVisible(0)}>
+            <Link to="/plan">Create your plan</Link>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Nav;
